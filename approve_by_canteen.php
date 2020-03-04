d07930f71d298591e013834915e34f1ff89f975b
praweenaka Ayeshmantha <praweenakahemachandra@gmail.com>
2020-03-04 10:25:39 +0530

    update project

--- approve_by_canteen.php
+++ approve_by_canteen.php
@@ -6,7 +6,7 @@ session_start();
 
     <div class="box box-header">
         <div class="box-header with-border ">
-            <h3 class="box-title"><b>Approve By Canteen</b></h3>
+            <h3 class="box-title"><b>Approve By Canteens</b></h3>
         </div>
         <form name= "form1" role="form" class="form-horizontal">
 
